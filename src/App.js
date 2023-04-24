a133d10d3edf9b9e5c24c8f413710e70678dd051
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-04-24 15:32:31 +0300

    addes a contact page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Header from "./components/student/Header";
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       
       <Routes>
         <Route path="/" element={<Header/>}/>
+        <Route path="/contact" element={<Contact/>}/>
       </Routes>
       
     </BrowserRouter>
