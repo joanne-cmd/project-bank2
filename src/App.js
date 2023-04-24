fe7f95a2a37df3427c024e0a7994b6ea74bb58e9
miluche <menjokipla@gmail.com>
2023-04-24 15:33:35 +0300

    Add cohort page, Attach cohortform to cohorts page

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-//import logo from './logo.svg';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Cohorts from './components/Cohorts/cohorts';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      
-      <Routes>
-        <Route path="/" element={<Cohorts/>}/>
-      </Routes>
-      
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-export default App;
+
+ export default App;
+//import logo from './logo.svg';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Cohorts from './components/Cohorts/cohorts';
+
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+      
+//       <Routes>
+//         <Route path="/" element={<Cohorts/>}/>
+//       </Routes>
+      
+//     </BrowserRouter>
+//   );
+// }
+// export default App;
