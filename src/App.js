e24544b5c74829ab687795dafd5f83137085d52e
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-04-24 21:57:55 +0300

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Header from "./components/student/Header";
 import Contact from './components/contact/Contact';
+import Login from './components/Login/login'
+import AddProject from './components/Addproject/addprojectform'
+import Logout from './components/Logout/logout';
 
 function App() {
   return (
@@ -9,6 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Header/>}/>
         <Route path="/contact" element={<Contact/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/add-project" element={<AddProject />} />
+        <Route path="/logout" element={<Logout />} />
       </Routes>
       
     </BrowserRouter>
