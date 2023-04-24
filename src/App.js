41b9a8d7f2a833b8393bf472c231d4ebc99eb92f
Pauline-njuguna <paulinenjuguna.w@gmail.com>
2023-04-24 15:45:39 +0300

    loginpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Header from "./components/student/Header";
-
+import Login from './components/Login/login'
 function App() {
   return (
     <BrowserRouter>
-      
       <Routes>
-        <Route path="/" element={<Header/>}/>
+        <Route path="/" element={<Header />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-      
     </BrowserRouter>
   );
 }
