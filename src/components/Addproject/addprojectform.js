c1b4e9595598357c1b4b756abb74aa86112730e1
Pauline-njuguna <paulinenjuguna.w@gmail.com>
2023-04-25 12:40:20 +0300

    changes navigation of the form

--- src/components/Addproject/addprojectform.js
+++ src/components/Addproject/addprojectform.js
@@ -49,8 +49,44 @@ const ProjectForm = () => {
       <div className="form-container">
         <h2>Add New Project</h2>
         <form onSubmit={handleSubmit} className="project-form">
-          {/* Form inputs go here */}
-          {/* ... */}
+          <label htmlFor="projectName">Project Name</label>
+          <input
+            type="text"
+            id="projectName"
+            placeholder="Project Name"
+            value={projectName}
+            onChange={(e) => setProjectName(e.target.value)}
+            required
+          />
+
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            value={description}
+            placeholder="Description"
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          ></textarea>
+          <label htmlFor="githubLink">GitHub Link</label>
+          <input
+            type="text"
+            id="githubLink"
+            placeholder="githubLink"
+            value={githubLink}
+            onChange={(e) => setGithubLink(e.target.value)}
+          />
+
+          <label>
+            <select value={course} onChange={(e) => setCourse(e.target.value)}>
+              <option value="">Select a course</option>
+              <option value="Android Development">Android Development</option>
+              <option value="Full-Stack Development">
+                Full-Stack Development
+              </option>
+              <option value="Data Science">Data Science</option>
+              <option value="Cybersecurity">Cybersecurity</option>
+            </select>
+          </label>
           <button type="submit">Submit</button>
         </form>
       </div>
