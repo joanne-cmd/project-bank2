5b5658f68dfcfd5d75a9a48bcd87292bfb640fcc
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-04-25 20:45:40 +0300

    change some styles

--- src/components/Logout/logout.js
+++ src/components/Logout/logout.js
@@ -6,10 +6,7 @@ const LogoutForm = ({ handleLogout }) => {
     <div className="logout-form-container">
       
       <div className="form-container">
-<<<<<<< HEAD
-=======
         <h2 className="goingout">Logout</h2>
->>>>>>> 73a42aaac353a0343c09ed5f98bff1e2979e9541
         <form onSubmit={handleLogout} className="logout-form">
           <p className="paragraph">Are you sure you want to logout?</p>
           <Link to="/"><button className="btnn__logout" type="submit">Logout</button> </Link>
