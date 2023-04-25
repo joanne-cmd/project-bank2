cecda7cf58b5ac5a899950f4b2b73107f89caeac
Pauline-njuguna <paulinenjuguna.w@gmail.com>
2023-04-25 09:30:29 +0300

    changes styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -31,7 +31,7 @@ export default function Footer() {
         </div>
       </section>
 
-      <section className=''>
+      <section className='footer'>
         <MDBContainer className=' text-center text-md-start mt-'>
           <MDBRow className='mt-3'>
             <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
